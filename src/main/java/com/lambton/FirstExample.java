cc33d34d902c9d83b7d21218476b36f7d3a6f19e
MacStudent <macstudent@128-IMAC26.local>
2020-01-21 20:48:49 -0500

    [Roop] Initial commit

---
+++ src/main/java/com/lambton/FirstExample.java
@@ -0,0 +1,10 @@
+package com.lambton;
+
+public class FirstExample {
+    public static void main(String[] args) {
+        System.out.println("welcome to java programming");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Roop Virk");
+        }
+    }
+}
