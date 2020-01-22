0482356e98f6d8205c3ccc99aa882e0e5aebadf7
macstudent <virkikroop13@gmail.com>
2020-01-22 18:48:43 -0500

    Day 2 commit

--- src/main/java/com/lambton/FirstExample.java
+++ src/main/java/com/lambton/FirstExample.java
@@ -8,6 +8,40 @@ public class FirstExample {
         }
         System.out.println("hiii");
         System.out.println("helloooo");
-        // sample - 1
+        // Day - 2 code starts here
+        int a = 100;
+        int b;
+        b=200;
+        int c=a+b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println("sum =" + a + b);
+        short x = 10000;
+        System.out.println("short =" + Short.MAX_VALUE);
+        System.out.println("short =" + Short.MIN_VALUE);
+
+        long y=500;
+        System.out.println(y);
+        System.out.println(Long.MAX_VALUE);
+        float f1= 1000.89F;
+        double d1=1000.78D;
+        System.out.println(f1);
+        System.out.println(d1);
+        int w=10/3;
+
+        float r=10/3.0F;
+        System.out.println(w);
+        System.out.println(r);
+        int r1=(int)(10.0f/3);
+        float r2=(float)(10/3);
+        System.out.println(r1);
+        System.out.println(r2);
+        int x1= 1_00_00_0/ 10;
+        double x2 = 1_00____50_60_5;
+        System.out.println("int value" + x1);
+        System.out.println("double value" + x2);
+
+
     }
 }
